7692c2e3cb74cab6931d63d1d89b8dbb71276c07
Rocky <498g0102@stust.edu.tw>
2017-10-15 01:29:28 +0800

    add legend

--- hw03/program_3_2.py
+++ hw03/program_3_2.py
@@ -6,54 +6,36 @@ pi = np.pi
 f_m = 30
 omega_m = 2*pi*f_m
 #F_T=1/T
-F_T = 2*f_m
-
-
+F_T = 3*f_m
+time_index1 = -1
+time_index2 = 1
 # t is the range of time, in which the analog signal is considered (Thus the analog singal is limimted in time, but unlimited in frequency) 
-t = np.arange(-1,2,1/F_T)
+t = np.arange(time_index1,time_index2+1/F_T,1/F_T)
 #continuous angular frequency after DTFT
-omega  =  np.arange(-omega_m-5,omega_m+5,0.05)
+omega = np.arange(-omega_m-5,omega_m+5,0.05)
 Gp = np.zeros(len(omega))
-# analog signal  page 51 
+# analog signal (page 51) 
 x = np.sin(omega_m *t)/(pi*t)
-
 for index ,element in enumerate(omega): 
     y = np.dot(x,np.exp(-1j*element*t))
     Gp[index]=y
 
 plt.subplot(2,1,1)
 plt.plot(omega,Gp)
+plt.title('$\omega_T$')
+plt.xlabel('$\omega$')
+plt.ylabel('$G_p(j\omega)$')
 plt.subplot(2,1,2)
-omega  =  np.arange(-3*omega_m-5,3*omega_m+5,0.05)
+omega = np.arange(-4*omega_m-5,4*omega_m+5,0.05)
 Gp = np.zeros(len(omega))
 # analog signal  page 51 
 x = np.sin(omega_m *t)/(pi*t)
-
 for index ,element in enumerate(omega): 
     y = np.dot(x,np.exp(-1j*element*t))
     Gp[index]=y
 plt.plot(omega,Gp)
-#p148 D/A
-#hold
-'''
-
-plt.subplot(3,1,1)
-plt.plot(t,x)
-plt.subplot(3,1,2)
-fs1=30
-#t1=np.arange(0,(1/freq)+1/fs,1/fs1)
-
-x1=np.sin(pi*fs1*t)/(pi*fs1*t)
-plt.plot(t,x1)
-plt.stem(t,x1)
-
-plt.subplot(3,1,3)
-
-n = len(x1)
-k = np.arange(n)
-T=n/fs1
-frq = k*T
-yf=fft(x1)
-plt.plot(2*pi*frq,np.abs(yf))
+plt.title('3 $\omega_T$')
+plt.xlabel('$\omega$')
+plt.ylabel('$G_p(j\omega)$')
+plt.tight_layout()
 plt.show()
-'''
--- hw03/program_3_3.py
+++ hw03/program_3_3.py
@@ -5,8 +5,6 @@ Created on Sat Oct 14 16:20:15 2017
 
 @author: rocky
 """
-#draw sin and sinc 
-#range variable 
 import numpy as np 
 import matplotlib.pyplot as plt 
 ''' analog signal'''
@@ -21,8 +19,10 @@ t = np.arange(time_index1,time_index2+1/fs,1/fs)
 #x = np.cos(omega_m*t) 
 x = np.sin(omega_m *t)/(pi*t)
 plt.subplot(3,1,1)
+plt.title('Continuous Signal ')
+plt.xlabel('Time(s)')
+plt.ylabel('Amplitude')
 plt.plot(t,x)
-
 '''sampling'''
 #F_T = 1/T
 F_T = 3.63*f_m
@@ -31,6 +31,9 @@ t_s = np.arange(time_index1,time_index2+1/F_T,1/F_T)  #sampling instances (nT)
 #samples =  np.cos(omega_m*t_s) #sampled version of analog signal
 samples = np.sin(omega_m *t_s)/(pi*t_s)
 plt.subplot(3,1,2)
+plt.title('Sampled')
+plt.xlabel('Time(s)')
+plt.ylabel('Amplitude')
 plt.stem(t_s,samples)
 plt.subplot(3,1,3)
 #cutoff freq of lowpass filter
@@ -40,8 +43,12 @@ DA = []
 for val in t:
     y =(np.sin(2*pi*F_C*(val-t_s)))/((2*pi*F_T*(val-t_s))/2)
     DA.append(np.dot(samples,y))
-plt.plot(t,x)
-plt.plot(t,DA,'r--')
+plt.title('D/A')
+plt.xlabel('Time(s)')
+plt.ylabel('Amplitude')
+plt.plot(t,x,label='Original Signal')
+plt.plot(t,DA,'r--',label='D/A')
+plt.legend()
 plt.tight_layout()
 plt.show()
 
