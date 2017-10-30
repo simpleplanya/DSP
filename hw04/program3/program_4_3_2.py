712ed980b2897a32610fade8e0e5809868c4389d
simpleplanya <simpleplanya@gmail.com>
2017-10-30 18:02:47 +0800

    change plot figure size

--- hw04/program3/program_4_3_2.py
+++ hw04/program3/program_4_3_2.py
@@ -1,38 +1,16 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Oct 26 13:45:22 2017
-
-@author: rocky
-"""
-
 import numpy as np 
 import matplotlib.pyplot as plt 
-
 x = np.array([1,2,3,4,5])
 #h1 is unit step function
 h1= np.array([1,1,1,1,1])
 y1=np.convolve(x,h1)
-h2=[1,-1,0,0,0]
-y2=np.convolve(y1[0:5],h2)
-
-plt.subplot(2,1,1)
-plt.title('$y_1[n]$')
 plt.xlabel('Time index')
 plt.ylabel('Amplitude')
-plt.stem(np.arange(len(y1[0:5])),y1[0:5])
-plt.xticks(range(0,5))
-plt.yticks(y1[0:5])
-plt.grid()
-
-plt.subplot(2,1,2)
-plt.title('$y_2[n]$')
-plt.xlabel('Time index')
-plt.ylabel('Amplitude')
-plt.stem(np.arange(len(y2[0:5])),y2[0:5])
-plt.xticks(range(0,5))
-plt.yticks(range(1,6))
-plt.grid()
+plt.stem(np.arange(len(x)),x,label='$x[n]$')
+plt.stem(np.arange(len(y1[0:5])),y1[0:5],label='$y_1[n]$',markerfmt='ro')
+#自動調整子圖間的間距
 plt.tight_layout()
+plt.grid()
+plt.legend()
 plt.show()
 
--- hw04/program4/program_4_4_2.py
+++ hw04/program4/program_4_4_2.py
@@ -9,14 +9,14 @@ Created on Thu Oct 26 14:23:15 2017
 import numpy as np 
 import matplotlib.pyplot as plt 
 pi = np.pi
-omega = 0.8*pi
+omega = 0.4
+#omega=1.2
 theda = 0
-h = np.array([1.6,1.5,-0.8])
-# = np.array([1])
-#sequence = np.arange(-1,1+0.05,0.05)
-sequence = np.arange(-1,1+0.05,0.05)
+h = np.array([-6.76195 ,13.456335, -6.76195])
+sequence = np.arange(0,100,1)
 x = np.cos(omega*sequence+theda)
 y = np.convolve(x,h)
+plt.title('$\omega_0=0.4$')
 plt.xlabel('Time index')
 plt.ylabel('Amplitude')
 plt.stem(sequence,x,label='x[n]')
