840484c37c3668d7097e74300911a2e9e9c21fb8
Rocky <498g0102@stust.edu.tw>
2017-11-18 22:24:25 +0800

    program6 reviw

--- hw05/program_6/program_6_1/program_6_1.py
+++ hw05/program_6/program_6_1/program_6_1.py
@@ -15,10 +15,11 @@ if __name__ == '__main__':
     omega_0 = 10*pi*10**-5
     seq = np.arange(0,20000)    
     x_n = np.cos(omega_0 *(seq*seq))
-    f, t, Sxx = signal.spectrogram(x_n,window='hamming',nperseg=200,noverlap=199)
+    f, t, Sxx = signal.spectrogram(x_n,fs=10000,window='hamming',nperseg=200,noverlap=199)
     plt.title('R=200')
     plt.pcolormesh(t, f, Sxx)   
     plt.colorbar()
+    plt.xlim(0,2)
     plt.ylabel('Frequency [Hz]')
     plt.xlabel('Time [sec]')
     plt.show()
--- hw05/program_6/program_6_1/program_6_2.py
+++ hw05/program_6/program_6_1/program_6_2.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Thu Nov 16 10:38:10 2017
+Created on Sat Nov 18 22:11:12 2017
 
-@author: rocky
+@author: Rocky
 """
+
 import numpy as np 
 import matplotlib.pyplot as plt 
 from scipy import signal
@@ -14,9 +14,9 @@ if __name__ == '__main__':
     pi = np.pi
     omega_0 = 10*pi*10**-5
     seq = np.arange(0,20000)    
-    x_n = np.cos(omega_0 *(seq*seq))
-    f, t, Sxx = signal.spectrogram(x_n,fs=10000,window='hamming',nperseg=50,noverlap=49,nfft=50)
-    plt.title('R = 50')
+    x_n = np.cos(omega_0 *(seq*seq*seq))
+    f, t, Sxx = signal.spectrogram(x_n,fs=10000,window='hamming',nperseg=200,noverlap=199)
+    plt.title('R=200')
     plt.pcolormesh(t, f, Sxx)   
     plt.colorbar()
     plt.xlim(0,2)
