027b9e3853670a867e92567aedd1ac011a4999c4
simpleplanya <simpleplanya@gmail.com>
2017-12-02 17:59:00 +0800

    change program5

--- hw06/program05/program5.py
+++ hw06/program05/program5.py
@@ -7,7 +7,13 @@ Created on Thu Nov 30 13:27:55 2017
 """
 
 from scipy.signal import residue 
+from scipy.signal import lfilter
+import numpy as np 
+#b = [2,0.8,0.5,0.3]
+#a = [1,0.8,0.2]
+b = [1,2.0]
+a=[1,0.4,-0.12]
 
-b = [2,0.8,0.5,0.3]
-a = [1,0.8,0.2]
-r,p,k=residue(b,a)
+#r,p,k=residue(b,a)
+x = [1,0,0,0,0,0,0,0,0,0,0]
+
