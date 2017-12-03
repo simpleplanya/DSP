1139b01560e550500f701ea29acb798c5c974957
Rocky <498g0102@stust.edu.tw>
2017-12-03 13:58:38 +0800

    change_API

--- hw06/program01/program_2.py
+++ hw06/program01/program_2.py
@@ -6,7 +6,7 @@ Created on Thu Nov 30 09:48:45 2017
 @author: rocky
 """
 from scipy.signal import tf2zpk
-from plot_zplane import zplane 
+from plot_zplane import draw_
 alpha=-0.45
 numerator   = [0,0,0,0,0,alpha**5,0,0,0,0,0,0,0,0,0,0,-alpha**16]
 denominator = [1,-alpha,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
@@ -14,4 +14,4 @@ denominator = [1,-alpha,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 #denominator = [1,0.6]
 
 z,p,k = tf2zpk(numerator,denominator)
-z,p=zplane(numerator,denominator)
+z,p=draw_(z,p)
