1366872480da5a3e80dfa002fc10f52b25e3572c
simpleplanya <simpleplanya@gmail.com>
2018-01-02 10:48:31 +0800

    modified

--- hw06/program01/program6_1_2.py
+++ hw06/program01/program6_1_2.py
@@ -12,9 +12,11 @@ np.set_printoptions(threshold=np.nan)
 def detection_zeroTozero(zero,pole):
     Cpole = np.copy(pole)
     Czero = np.copy(zero)
+    #針對pole進行偵測
     for idx,val in  enumerate (pole):
         if val in np.round(np.real(zero),3):
-            Cpole=np.delete(Cpole,idx)      
+            Cpole=np.delete(Cpole,idx)
+	 #針對zero進行偵測      
     for Zidx,Zval in  enumerate (np.round(np.real(zero),3)):
 
         if Zval in np.round(pole,3):
@@ -22,11 +24,12 @@ def detection_zeroTozero(zero,pole):
     return  Czero,Cpole 
 
 
-
+#參數初始化
 alpha=-0.45
 numerator   = [0,0,0,0,0,alpha**5,0,0,0,0,0,0,0,0,0,0,-alpha**16]
 denominator = [1,-alpha,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 z,p,k = tf2zpk(numerator,denominator)
+#0分之0型判別
 z,p = detection_zeroTozero(z,p)
 
 print ('zero=', z)
